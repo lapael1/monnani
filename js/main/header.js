7c3fb3cdb66076d2b1b2bda1a50c61c475fbe06d
kwakjkh <1210kwakjkh@gmail.com>
2024-10-16 16:00:56 +0900

    header 수정 및 로그인, 회원가입 페이지 작업 중

--- js/main/header.js
+++ js/main/header.js
@@ -1,7 +1,15 @@
 // js
 window.onload = function () {
 const header = document.querySelector(".header")
-document.addEventListener("")
+function headerScrollCss(){
+    const headerHeight = header.offsetHeight
+    if (window.scrollY >= headerHeight){
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+}
+window.addEventListener("scroll",headerScrollCss)
 }
 // jquery
 $(document).ready(function () {
