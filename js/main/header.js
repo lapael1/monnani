7c79c51f8976a22ad65f6803837178afb34c5771
kwakjkh <1210kwakjkh@gmail.com>
2024-10-17 12:13:11 +0900

    login, index 페이지 간 상호작용 작업 중

--- js/main/header.js
+++ js/main/header.js
@@ -1,17 +1,30 @@
 // js
 window.onload = function () {
-const header = document.querySelector(".header")
-function headerScrollCss(){
-    const headerHeight = header.offsetHeight
-    if (window.scrollY >= headerHeight){
-        header.classList.add("active")
-    }else{
-        header.classList.remove("active")
+  const header = document.querySelector(".header");
+  function headerScrollCss() {
+    const headerHeight = header.offsetHeight;
+    if (window.scrollY >= headerHeight) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
     }
-}
-window.addEventListener("scroll",headerScrollCss)
-}
-// jquery
-$(document).ready(function () {
+  }
+  window.addEventListener("scroll", headerScrollCss);
 
-})
+  // 로그인
+  function showInitDashboard() {
+    let params = new URLSearchParams(window.location.search);
+    let name = params.get("name");
+    if (name) {
+      document.getElementById("login-button").style.display = "none";
+      document.getElementById("signup-button").style.display = "none";
+      document.getElementById("login-display").style.display = "block";
+      document.getElementById(
+        "name-display"
+      ).textContent = `${name}님 환영합니다/
+`;
+    }
+  }
+};
+// jquery
+$(document).ready(function () {});
